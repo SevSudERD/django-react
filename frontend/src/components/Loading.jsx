c08e7cf3de5b8296ff94e401c8676c41ec01d83a
SevSudERD <161018073+SevSudERD@users.noreply.github.com>
2024-07-21 06:08:02 +0300

    first commit

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import "../styles/Loading.css"
+
+const LoadingIndicator = () => {
+    return <div className="loading-container">
+        <div className="loader"></div>
+    </div>
+}
+
+export default LoadingIndicator
---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import "../styles/Note.css"
+
+function Note({ note, onDelete }) {
+    const formattedDate = new Date(note.created_at).toLocaleDateString("en-US")
+
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{formattedDate}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    );
+}
+
+export default Note
